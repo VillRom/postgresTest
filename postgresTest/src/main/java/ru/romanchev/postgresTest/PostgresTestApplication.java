957f3a5887d6daffed883412243e9990f49c73a1
VillRom <alexromanchev@mail.ru>
2025-02-20 22:35:42 +0400

    Create project

---
+++ postgresTest/src/main/java/ru/romanchev/postgresTest/PostgresTestApplication.java
@@ -0,0 +1,13 @@
+package ru.romanchev.postgresTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresTestApplication.class, args);
+	}
+
+}
---
+++ postgresTest/src/main/java/ru/romanchev/postgresTest/model/User.java
@@ -0,0 +1,16 @@
+package ru.romanchev.postgresTest.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Long number;
+    private String nikname;
+}
